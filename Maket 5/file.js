f3165ea9597a3c1569a3b5f11b67d4136cc9b4cf
bgldjrtxmn <94174867+bgldjrtxmn@users.noreply.github.com>
2022-10-04 10:49:45 +0300

    Layout
    Layout

---
+++ Maket 5/file.js
@@ -0,0 +1,3 @@
+let a = document.querySelector('.img__NOmain');
+let b = document.querySelector('.huinya');
+a.onclick = () => { b.style.display === 'none' ? b.style.display = 'block' : b.style.display = 'none'; }
